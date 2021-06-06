fdc60cb0606420f5d5710627e94a0b1a3d6882fb
margauxdufresne <72607017+margauxdufresne@users.noreply.github.com>
2021-06-06 22:18:44 +0200

    Update sobel.c

--- src/sobel.c
+++ src/sobel.c
@@ -56,9 +56,9 @@ void filtre_sobel(){
 
 int main(){
     char* nom_fic = "images/tournesol.jpeg";
-    load_image_data(nom_fic);//entrée de l'image 1
+    charger_image_data(nom_fic);//entrée de l'image 1
     filtre_sobel(); //filtre sobel appliqué à l'image 1
-    save_image_data(nom_fic);// sortie de limage 2
+    save_image(nom_fic);// sortie de limage 2
     
     return 0;
 }
