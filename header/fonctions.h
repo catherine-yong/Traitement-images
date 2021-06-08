752d5209b67178079c025c0ac279bf6a82c8489e
Catherine Yong <47810291+catherine-yong@users.noreply.github.com>
2021-06-08 20:20:58 +0200

    Add files via upload

--- header/fonctions.h
+++ header/fonctions.h
@@ -12,7 +12,7 @@ float niv_gris(const SDL_Surface *image , int X, int Y);
 int *couleur_image(SDL_Surface *image, int *color_image) ;
 float mse_images(SDL_Surface *image1 , SDL_Surface *image2) ;
 SDL_Surface *moy_pixel(SDL_Surface *image, float eps);
-SDL_Surface *correction_image(SDL_Surface *image, Uint32 nb_sub,Uint32 eps);
+SDL_Surface *corr_image(SDL_Surface *image, Uint32 nb_sub,Uint32 eps);
 int similaires(SDL_Surface *image1, SDL_Surface *image2);
 int main();
 
